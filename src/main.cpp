74fb22ab890c74dc82f28ff59531787b1b180293
rdrink <rdrink@gmail.com>
2025-08-07 15:19:57 -0500

    added temp and water sensor code

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #include <Wire.h>
 #include "SparkFun_Qwiic_OpenLog_Arduino_Library.h"
 #include "location.h"
+#include "DS2482-RK.h"
 OpenLog sdLog; //Create instance
 LocationPoint point = {};
 
@@ -36,7 +37,9 @@ float averageVoltage = 0,tdsValue = 0,temperature = 25;
 #define sampleInterval 40U
 #define logInterval 3*1000U
 
-#define gpsBaud 9600
+#define waterPin D3 // 100k pulldown to gnd (pin 34)
+
+DS2482 ds(Wire, 0);
 
 // #define DEBUGGING // uncomment for serial debugging; comment for logging
 
@@ -50,11 +53,15 @@ float turbiditySensorValue = -0;
 float turbidityMin = 999;
 float turbidityMax = -999;
 
+float tempValue = -999;
+
+int senseWater = 0;
+
 String date_time;
 char buf[256];
 long lat = 0.0, lon = 0.0; 
 
-String formatCSV(const String &date, int tMed, float turbidity) { // These -> will have to be manually expanded for now
+String formatCSV(const String &date, int tMed, float turbidity, float temp) { // These -> will have to be manually expanded for now
     String csv_data =
         "Date: " + date + "," +
         "Latitude: " + point.latitude + "," +
@@ -63,19 +70,21 @@ String formatCSV(const String &date, int tMed, float turbidity) { // These -> wi
         "Course: " + point.heading + "," +
         "TDS Median: " + String(tMed) + "," +
         "Turbidity: "+ String(turbidity);
+        "Temp C°: "+ String(temp);
     return csv_data;
 }
 
 // old school JSON //
-String formatJSON(const String &date, int tMed, float turbidity) { // These -> will have to be manually expanded for now
+String formatJSON(const String &date, int tMed, float turbidity, float temp) { // These -> will have to be manually expanded for now
     String json_data = String::format(
-        "{\"date\":%s,\"lat\":%f,\"lng\":%f,\"speed\":%f,\"course\":%f,\"tdsMedian\":%f,\"turbidity\":%f}",
+        "{\"date\":%s,\"lat\":%f,\"lng\":%f,\"speed\":%f,\"course\":%f,\"tdsMedian\":%f,\"turbidity\":%f\"temp\":%f}",
         date.c_str(),
         point.latitude, point.longitude,
         point.speed,
         point.heading,
         tMed,
-        turbidity
+        turbidity,
+        temp
     ).c_str();
     return json_data;
 }
@@ -86,6 +95,39 @@ void logToSD(String info) {
     sdLog.syncFile();
 }
 
+float tdsCompensation(float value) {
+      float compensationCoefficient=1.0+0.02*(temperature-25.0);    //temperature compensation formula: fFinalResult(25^C) = fFinalResult(current)/(1.0+0.02*(fTP-25.0));
+      // -- should use actual water temp here -- //
+      float compensationVolatge=value/compensationCoefficient;  //temperature compensation
+      //tdsValue=(133.42*compensationVolatge*compensationVolatge*compensationVolatge - 255.86*compensationVolatge*compensationVolatge + 857.39*compensationVolatge)*0.5; //convert voltage value to tds value
+      tdsValue=(133.42* pow(compensationVolatge,3)- 255.86* pow(compensationVolatge, 2) + 857.39*compensationVolatge)*0.5; //convert voltage value to tds value
+      return tdsValue;
+}
+
+int getMedianNum(int bArray[], int iFilterLen) 
+{
+      int bTab[iFilterLen];
+      for (byte i = 0; i<iFilterLen; i++)
+	  bTab[i] = bArray[i];
+      int i, j, bTemp;
+      for (j = 0; j < iFilterLen - 1; j++) 
+      {
+	  for (i = 0; i < iFilterLen - j - 1; i++) 
+          {
+	    if (bTab[i] > bTab[i + 1]) 
+            {
+		bTemp = bTab[i];
+	        bTab[i] = bTab[i + 1];
+		bTab[i + 1] = bTemp;
+	     }
+	  }
+      }
+      if ((iFilterLen & 1) > 0)
+	bTemp = bTab[(iFilterLen - 1) / 2];
+      else
+	bTemp = (bTab[iFilterLen / 2] + bTab[iFilterLen / 2 - 1]) / 2;
+      return bTemp;
+}
 
 void setup() {
     Wire.begin(); //Initialize I2C
@@ -102,6 +144,11 @@ void setup() {
     LocationConfiguration config;
     config.enableAntennaPower(GNSS_ANT_PWR);
     Location.begin(config);
+
+    ds.setup();
+	  DS2482DeviceReset::run(ds, nullptr);
+
+    pinMode(D3, INPUT);
 }
 
 
@@ -146,58 +193,45 @@ void loop() {
  
       /***** Turbidity *****/
       float turbidityValue = turbiditySensorValue * (5.0 / 1024.0); // ref volatge / 2^12
+
+      /***** Temp *****/
+      DS24821WireAddress addr;
+      DS2482GetTemperatureCommand::run(ds, addr, [](DS2482GetTemperatureCommand&, int status, float tempC) {
+        if (status != DS2482Command::RESULT_DONE) {
+          return;
+        }
+        tempValue = tempC;
+        //Serial.printlnf("temperature: %f C", tempValue);
+      });
+
       
       String date = Time.timeStr();
       
       #ifndef DEBUGGING // LOG
       // choose format
       //String ldata = formatJSON(date, tdsMedian, turbidityValue);
-      String ldata = formatCSV(date, tdsValue, turbidityValue);
+      String ldata = formatCSV(date, tdsValue, turbidityValue, tempValue);
       logToSD(ldata);
       #endif
       
       #ifdef DEBUGGING // SERIAL
       //String sdata = formatJSON(date, tdsMedian, turbidityValue);
-      String sdata = formatCSV(date, tdsValue, turbidityValue);
+      String sdata = formatCSV(date, tdsValue, turbidityValue, tempValue);
       Serial.println(sdata);  
       #endif
       
       //-- reset values --//
       //turbMin
    }
+
+   ds.loop(); // temp sensor
+
+  senseWater = digitalRead(waterPin); 
+  if(senseWater == 1) {
+    Log.info("WATER!");
+   delay(250);
+  }
+  
 }
 
-float tdsCompensation(float value) {
-      float compensationCoefficient=1.0+0.02*(temperature-25.0);    //temperature compensation formula: fFinalResult(25^C) = fFinalResult(current)/(1.0+0.02*(fTP-25.0));
-      // -- should use actual water temp here -- //
-      float compensationVolatge=value/compensationCoefficient;  //temperature compensation
-      //tdsValue=(133.42*compensationVolatge*compensationVolatge*compensationVolatge - 255.86*compensationVolatge*compensationVolatge + 857.39*compensationVolatge)*0.5; //convert voltage value to tds value
-      tdsValue=(133.42* pow(compensationVolatge,3)- 255.86* pow(compensationVolatge, 2) + 857.39*compensationVolatge)*0.5; //convert voltage value to tds value
-      return tdsValue;
-}
-
-int getMedianNum(int bArray[], int iFilterLen) 
-{
-      int bTab[iFilterLen];
-      for (byte i = 0; i<iFilterLen; i++)
-	  bTab[i] = bArray[i];
-      int i, j, bTemp;
-      for (j = 0; j < iFilterLen - 1; j++) 
-      {
-	  for (i = 0; i < iFilterLen - j - 1; i++) 
-          {
-	    if (bTab[i] > bTab[i + 1]) 
-            {
-		bTemp = bTab[i];
-	        bTab[i] = bTab[i + 1];
-		bTab[i + 1] = bTemp;
-	     }
-	  }
-      }
-      if ((iFilterLen & 1) > 0)
-	bTemp = bTab[(iFilterLen - 1) / 2];
-      else
-	bTemp = (bTab[iFilterLen / 2] + bTab[iFilterLen / 2 - 1]) / 2;
-      return bTemp;
-}
   
