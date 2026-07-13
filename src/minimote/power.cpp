899b9de4e9e6ffc78ce501b26b5f93b99b5ab00a
Myles <43725835+MylesAndMore@users.noreply.github.com>
2026-07-13 10:08:41 -0500

    reduce deep sleep time

--- src/minimote/power.cpp
+++ src/minimote/power.cpp
@@ -20,7 +20,7 @@
 #include "pmu.h"
 
 #define LOW_BATTERY_VOLT 3.65f // Low power mode will be entered under this voltage
-#define LOW_BATTERY_SLEEP_SEC 21600 // Time to deep sleep when low battery is detected, in seconds (6 hours)
+#define LOW_BATTERY_SLEEP_SEC 3600 // Time to deep sleep when low battery is detected, in seconds (1 hour)
 
 #define SAMPLE_SLOT_SECONDS 180 // Duration to sleep between samples, in seconds (3 minutes). Should be an even divisor of PUBLISH_SLOT_SECONDS
 #define PUBLISH_SLOT_SECONDS 900 // Duration to sleep between publish windows, in seconds (15 minutes). Can be changed by sending a control message
